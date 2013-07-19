a1ce37e2e41e019cb1c13f6cee92939d077efcbf
Nathan Long <nathan.long@paceco.com>
2013-07-19 06:46:18 -0400

    removed bad ultisnip reference, added uninstall script

---
+++ bin/uninstall.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+DOTFILES_DIRECTORY="${HOME}/.dotfiles"
+
+cd ${DOTFILES_DIRECTORY}
+
+read -p "This will completely remove all dotfiles and references. Are you sure? (y/n) " -n 1
+echo
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sudo rm -r ${DOTFILES_DIRECTORY}
+    rm ${HOME}/.bashrc
+    rm ${HOME}/.bash_profile
+    rm ${HOME}/.inputrc
+    rm ${HOME}/.gitconfig
+    rm ${HOME}/.vim
+    rm ${HOME}/.gvimrc
+    rm ${HOME}/.vimrc
+
+    echo "Dotfiles removed."
+fi
+
