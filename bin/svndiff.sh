92a8affb4edfd617f6be92e46eae48adb820342f
Nathan Long <nlong5@gmail.com>
2016-09-15 13:39:00 -0400

    changing things up, new colorscheme, new plugins, svn support and tools, etc

---
+++ bin/svndiff.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Configure your favorite diff program here.
+DIFF="/usr/bin/vimdiff"
+
+# Subversion provides the paths we need as the sixth and seventh
+# parameters.
+LEFT=${6}
+RIGHT=${7}
+
+# Call the diff command
+$DIFF $LEFT $RIGHT
