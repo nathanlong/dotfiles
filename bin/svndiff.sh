57d26540ead8dfebd226aee8f0889f152ed4922a
Nathan Long <nlong5@gmail.com>
2017-11-16 09:57:40 -0500

    Small updates and tweaks

--- bin/svndiff.sh
+++ bin/svndiff.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Configure your favorite diff program here.
-DIFF="vimdiff"
+DIFF="vimdiff --not-a-term"
 
 # Subversion provides the paths we need as the sixth and seventh
 # parameters.
